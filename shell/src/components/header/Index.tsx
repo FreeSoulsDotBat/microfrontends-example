003a4ab58a38f078fc38803d7d5f5191bb2f1702
jonatan.bossan <jonatan.bossan@akna.com>
2022-06-01 18:37:49 -0300

    Header

--- shell/src/components/header/Index.tsx
+++ shell/src/components/header/Index.tsx
@@ -1,5 +1,13 @@
 import React from 'react'
+import './style.scss'
 
 export const Header = () => {
-	return <div>Header</div>
+	return (
+		<React.Fragment>
+			<header id="header">
+				Header from Shell
+				<button>Login</button>
+			</header>
+		</React.Fragment>
+	)
 }
