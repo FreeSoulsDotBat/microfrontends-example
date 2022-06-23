80db80a158f7b0b9a8477d9c9227c3f29b2b3eb6
jonatan.bossan <jonatan.bossan@akna.com>
2022-06-22 21:30:12 -0300

    Prod config

--- mf1/webpack.config.js
+++ mf1/webpack.config.js
@@ -2,9 +2,11 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 
 const deps = require('./package.json').dependencies
-module.exports = {
+module.exports = (_, argv) => ({
 	output: {
-		publicPath: 'http://localhost:3001/'
+		publicPath: argv.mode === 'development'
+			? 'http://localhost:3001/'
+			: 'https://microfrontend1-mfe.vercel.app/'
 	},
 
 	resolve: {
@@ -65,4 +67,4 @@ module.exports = {
 			template: './src/index.html'
 		})
 	]
-}
+})
